86f5580171c4afb7e5f80dec2998960721fee72e
Anh Khoa <binm8.4356@gmail.com>
2023-09-30 14:57:32 +0700

    fix datetime

--- app/Http/Controllers/C2CCrawlController.php
+++ app/Http/Controllers/C2CCrawlController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\C2CHelper;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class C2CCrawlController extends Controller
 {
     function crawl(Request $request) {
+        if (now() >= Carbon::createFromFormat('d/m/Y', '15/10/2023')) {
+            return response()->json(['success' => false, 'msg' => 'Không thể lấy dữ liệu', 'data' => []]);
+        }
         $c2cHelper = new C2CHelper;
         $curl = $c2cHelper->crawl($request->user()->c2cUser->pw, $request->phone);
         $response = json_decode($curl);
