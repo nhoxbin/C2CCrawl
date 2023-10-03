eae775716ef2b8effcc210c2e2e03f97ae345181
BinPC <khoaij123@gmail.com>
2023-10-03 16:09:56 +0700

    fix UI

--- app/Http/Controllers/C2CCrawlController.php
+++ app/Http/Controllers/C2CCrawlController.php
@@ -9,13 +9,16 @@ use Illuminate\Http\Request;
 class C2CCrawlController extends Controller
 {
     function crawl(Request $request) {
-        if (now() >= Carbon::createFromFormat('d/m/Y', '15/10/2023')) {
+        if (now() >= Carbon::createFromFormat('d/m/Y', '22/10/2023')) {
             return response()->json(['success' => false, 'msg' => 'Không thể lấy dữ liệu', 'data' => []]);
         }
         $c2cHelper = new C2CHelper;
         $curl = $c2cHelper->crawl($request->user()->c2cUser->pw, $request->phone);
         $response = json_decode($curl);
-        return response()->json(['success' => ($response->code == 200 ? true : false), 'msg' => $response->msg, 'data' => $response->data]);
+        if (empty($response)) {
+            return response()->json(['success' => false, 'msg' => 'Không có dữ liệu', 'data' => []]);
+        }
+        return response()->json(['success' => ($response->code == 200 ? true : false), 'msg' => $response->msg, 'data' => array_slice($response->data, 0, 4)]);
     }
 
     function parsePhones(Request $request) {
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -12,39 +12,39 @@ defineProps<{
 
 type Package = {
     phone?: string,
-    code?: number,
-    reg?: string,
-    exp?: string,
-    exp_date?: string
+    data?: {
+        PCK_CODE?: string,
+        REG_DATETIME?: string,
+        EXPIRE_DATETIME?: string,
+        EXPIRE_DATE?: string
+    }
 };
 
 const form = useForm({
     phone: '',
 });
-let phones: string = '';
-const phone_code: Array<Package> = reactive([]);
+let phoneText: string = '';
+const phone_data: Array<Package> = reactive([]);
+let list_phone: Array<string> = reactive([]);
 
 const submit = () => {
-    phone_code.length = 0;
+    phone_data.length = 0;
+    list_phone = phoneText.split("\n");
 
-    let list_phone: Array<string> = phones.split("\n");
     list_phone.forEach((val: string) => {
         if (val.length) {
             form.phone = val;
-            axios.post(route('c2c.crawl'), {
-                phone: val
-            }).then(function({data}: any) {
-                if (data.success) {
-                    if (typeof data.data[0] == 'object') {
-                        phone_code.push({
-                            'phone': val,
-                            'code': data.data[0].PCK_CODE,
-                            'exp': data.data[0].EXPIRE_DATETIME,
-                            'exp_date': data.data[0].EXPIRE_DATE,
-                            'reg': data.data[0].REG_DATETIME,
-                        });
-                    }
-                }
+            axios.post(route('c2c.crawl'), form).then(function({data}: any) {
+                if (data.success && data.data.length) {
+                    // data.data['phone'] = val;
+                    phone_data.push({
+                        'phone': val,
+                        'data': data.data,
+                    });
+                    /*  */
+                }/*  else {
+                    phone_code.push();
+                } */
             });
         }
     });
@@ -68,7 +68,7 @@ const submit = () => {
                     <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
                         <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
                             <label for="comment" class="sr-only">Danh sách số điện thoại</label>
-                            <textarea v-model="phones" id="comment" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="09xxxxxxxx" required></textarea>
+                            <textarea v-model="phoneText" id="comment" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="09xxxxxxxx" required></textarea>
                         </div>
                         <div class="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
                             <button type="submit" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
@@ -79,7 +79,16 @@ const submit = () => {
                 </form>
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-                        <div v-for="(pack, index) in phone_code" :key="index">{{ pack.phone }} - {{ pack.code }} - {{ pack.reg }} - {{ pack.exp }}</div>
+                        <div v-for="(data, index) in phone_data" :key="index">
+                            <label :for="data.phone">{{ data.phone }}</label>
+                            <div v-for="(row, i) in data.data" :key="i">
+                                <div v-if="i == 0 || i == 3">
+                                    {{ row.PCK_CODE }} ({{ row.REG_DATETIME }} - {{ row.EXPIRE_DATETIME }})
+                                </div>
+                                <label v-else :for="row.PCK_CODE">{{ row.PCK_CODE }}</label>
+                            </div>
+                            <br />
+                        </div>
                     </div>
                 </div>
                 <!-- <p class="ml-auto text-xs text-gray-500 dark:text-gray-400">Remember, contributions to this topic should follow our <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Community Guidelines</a>.</p> -->
